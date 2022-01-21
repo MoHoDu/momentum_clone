1441e5e8ea7f193a766cbaab408673a6ff3cbd26
MoHoDu <imvagueness@gmail.com>
2022-01-21 12:45:14 +0900

    make_a_trainthought_page

---
+++ trainTought02/main.js
@@ -0,0 +1,9 @@
+const objects = {
+    searchForm: document.body.querySelector(".outBox .searchline .searchform"),
+    searchBar: searchForm.querySelector("#searchbar")
+};
+
+const handlers = {
+
+};
+
