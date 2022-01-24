c284669b728b011ca85ef1f253d620725e6ac382
MoHoDu <imvagueness@gmail.com>
2022-01-24 10:45:25 +0900

    train_your_thought

---
+++ Algorithm/algorithm01.py
@@ -0,0 +1,45 @@
+# 순차 탐색
+def seqsearch (n, S, x):
+    location = 1
+    while (location <= n and S[location] != x):
+        location += 1
+    if (location > n) :
+        location = 0
+    return location
+
+# 합 구하기
+S = [0, 10, 7, 11, 5, 13, 8]
+x = 5
+location = seqsearch(len(S) - 1, S, x)
+# pseudo-code를 썼으므로 인덱스 0 값은 제외
+print('location =', location)
+
+def sum(n, S):
+    result = 0
+    for i in range(1, n + 1):
+        result += S[i]
+    return result
+
+S = [-1, 10, 7, 11, 5, 13, 8]
+sum_value = sum(len(S) - 1, S)
+print('sum =', sum_value)
+
+# 비내림차순(오름차순) 정렬: 교환 정렬 알고리즘
+def exchange(S):
+    n = len(S) #실제 n = n-1
+    for i in range(n-1): # i부터 n-2까지 (슈도코드 0 빼니까)
+        for j in range(i + 1, n): # i+1부터 n-1까지
+            if(S[i] > S[j]):
+                S[i], S[j] = S[j], S[i] #파이썬에서의 swap
+            #     print(S, 'v', S[i])
+            # else:
+            #     print(S, 'x', S[i])
+    return S
+
+S = [-1, 10, 7, 11, 5, 13, 8]
+
+print('Before=', S)
+sortedS = exchange(S)
+print('After =', sortedS)
+
+#행렬의 곱셈
